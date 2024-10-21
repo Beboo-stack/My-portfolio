70adfc9f7e73b064632f366e076fc31a9f5302a8
Beboo-stack <peteralber735@gmail.com>
2024-10-21 06:39:14 +0300

    pixel

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import TransitionProvider from "@/components/transitionProvider";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,9 +14,58 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <script
+          id="Meta_Pixel_Code"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '27574582028822450');
+fbq('track', 'PageView');
+      `,
+          }}
+        />
+        <noscript>
+          <Image
+            height="1"
+            width="1"
+            style="display:none"
+            alt=""
+            src="https://www.facebook.com/tr?id=27574582028822450&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </head>
       <body className={inter.className}>
         <TransitionProvider>{children}</TransitionProvider>
       </body>
     </html>
   );
 }
+
+
+// <!-- Meta Pixel Code -->
+// <script>
+// !function(f,b,e,v,n,t,s)
+// {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+// n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+// if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+// n.queue=[];t=b.createElement(e);t.async=!0;
+// t.src=v;s=b.getElementsByTagName(e)[0];
+// s.parentNode.insertBefore(t,s)}(window, document,'script',
+// 'https://connect.facebook.net/en_US/fbevents.js');
+// fbq('init', '27574582028822450');
+// fbq('track', 'PageView');
+// </script>
+// <noscript><img height="1" width="1" style="display:none"
+// src="https://www.facebook.com/tr?id=27574582028822450&ev=PageView&noscript=1"
+// /></noscript>
+// <!-- End Meta Pixel Code -->
